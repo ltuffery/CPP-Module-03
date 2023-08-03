c807a4186bdcea860ca9af8543911921fd7b8f5c
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-08-03 15:01:30 +0200

    fix ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,15 +1,22 @@
 #include "ClapTrap.h"
 #include "ScavTrap.h"
+#include "FragTrap.h"
 #include <iostream>
 
 int main() {
 	ScavTrap scav("clap");
 	ClapTrap clap("trap");
+	FragTrap frag("frag");
 
 	scav.attack("trap");
 	clap.takeDamage(20);
 	for (int i = 0; i < 10; i++) {
 		clap.beRepaired(1);
 	}
-	clap.attack("clap");
+	clap.attack("trap");
+	frag.attack("clap");
+	scav.takeDamage(30);
+	scav.attack("frag");
+	frag.takeDamage(20);
+	frag.highFivesGuys();
 }
