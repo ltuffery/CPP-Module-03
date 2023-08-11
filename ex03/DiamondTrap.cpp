015db098f449dd9eee0f111f1d5bcfaf25d87e70
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-08-11 16:50:30 +0200

    fix heritage

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -4,6 +4,17 @@
 #include "ScavTrap.h"
 #include <string>
 
-DiamondTrap::DiamondTrap(std::string name): ClapTrap(name + "_clap_trap") {
+DiamondTrap::DiamondTrap(void) : ClapTrap("default_clap_trap") {
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
+}
 
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_trap") {
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
+}
+
+DiamondTrap::~DiamondTrap(void) {
 }
